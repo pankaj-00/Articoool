c0ae633e5797a0cdc50674c9efb57e33bb901696
Pankaj Jangir <pankajjangir20@cse.iiitp.ac.in>
2021-12-23 19:53:28 +0530

    App/index update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Copyright from "./Components/copyright";
 import Navbar from "./Components/navbar";
 function App() {
   return (
-    <>
+    <div className="ContainerM">
       <Navbar />
-    </>
+      <Copyright />
+    </div>
   );
 }
 

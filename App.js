6258f5b82586b45cbb56306395fc7c41d724d2ea
Pankaj Jangir <75432626+lucifer-nc0@users.noreply.github.com>
2021-12-23 17:22:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Copyright from "./Components/copyright";
+import Navbar from "./Components/navbar";
+function App() {
+  return (
+    <div className="ContainerM">
+      <Navbar />
+      <Copyright />
+    </div>
+  );
+}
+
+export default App;

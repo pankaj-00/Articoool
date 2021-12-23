6df3c561dc5f73438f195123a58dc6f1976fa7ef
Pankaj Jangir <pankajjangir20@cse.iiitp.ac.in>
2021-12-23 18:57:28 +0530

    Changes

---
+++ src/Components/copyright.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Copyright = () => {
+  return (
+    <div className="CopyrightContainer">
+      Copyright © LuciferDanceAcademy.com | All Rights Reserved
+    </div>
+  );
+};
+
+export default Copyright;
